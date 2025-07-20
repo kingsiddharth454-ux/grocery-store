4b1f87bd0436fdd3c1e844d35be2714e9c2b8c19
kingsiddharth454-ux <kingsiddharth454@gmail.com>
2025-07-20 10:52:13 +0530

    Add files via upload

---
+++ kirana.js
@@ -0,0 +1,45 @@
+let searchform= document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick =()=>
+{
+    searchform.classList.toggle('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+
+let shoppingcart= document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick =()=>
+{
+    shoppingcart.classList.toggle('active');
+    searchform.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+let loginform= document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick =()=>
+{
+    loginform.classList.toggle('active');
+    searchform.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+let navbar= document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick =()=>
+{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+
+}
+window.onscroll =()=>
+{
+    searchform.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
